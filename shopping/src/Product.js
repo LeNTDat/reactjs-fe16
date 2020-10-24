2c63edfcf104d2b9f84dbc0a5b9ec0481be6f6eb
cm002 <lehongphuc7394@gmail.com>
2020-10-24 09:40:43 +0700

    create component

---
+++ shopping/src/Product.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Person() {
+  return (
+    <p>PRODUCT ~~~~ !!!</p> //JSX
+  );
+}
+
+export function AnotherPerson(){
+    return(
+        <p>ANOTHERHRRRR</p>
+    )
+}
+
